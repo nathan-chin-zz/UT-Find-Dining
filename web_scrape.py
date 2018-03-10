21bb77e8106cd10980a00b32a6bfdb2f52ebaeb3
nathan-chin <nathanchin@utexas.edu>
2018-03-09 18:48:19 -0600

    Added dining hours

--- web_scrape.py
+++ web_scrape.py
@@ -10,6 +10,10 @@ import urllib3
 from bs4 import BeautifulSoup
 from enum import Enum
 
+def getChoice():
+    print()
+    # See menu(s), where should you go
+
 def printName(name, symbol):
     loop = 0
     if symbol == '#': print()
@@ -40,6 +44,17 @@ class LOCATION(Enum):
 BASE_URL = 'http://hf-food.austin.utexas.edu/foodpro/'
 LOCATIONS_URL = 'location2.asp'
 LOCATIONS = list(LOCATION)
+LOCATION_HOURS = {
+    LOCATION.Jester_City_Limits.name: ['Monday-Thursday | 7am-11pm', 'Friday | 7am-9pm', 'Saturday | 9am-8pm', 'Sunday | 9am-11pm'],
+    LOCATION.Jester_City_Market.name: ['Monday-Thursday | 7am-Midnight', 'Friday | 7am-9pm', 'Saturday | 2pm-8pm', 'Sunday | 2pm-Midnight'],
+    LOCATION.Jest_A_Pizza.name: ['Monday-Thursday | 11am-Midnight', 'Friday | 11am-2pm', 'Saturday | Closed', 'Sunday | 5pm-Midnight'],
+    LOCATION.J2.name: ['Monday-Friday | 10:30am-8pm', 'Saturday-Sunday | Closed'],
+    LOCATION.J2_FAST.name: ['Monday-Friday | 10:30am-8pm', 'Saturday-Sunday | Closed'],
+    LOCATION.Kinsolving.name: ['Monday-Friday | 10:30am-8pm', 'Saturday (and non-class days) | 11am-2pm, 4:30pm-7pm', 'Sunday | 11am-2pm'],
+    LOCATION.Kins_Market.name: ['Monday-Thursday | 7am-11pm', 'Friday | 7am-3pm', 'Saturday | 3pm-7pm', 'Sunday | 4pm-11pm'],
+    LOCATION.Cypress_Bend_Cafe.name: ['Monday-Thursday | 7am-9pm', 'Friday | 7am-2pm', 'Saturday-Sunday | 12pm-7pm'],
+    LOCATION.Littlefield_Patio_Cafe.name: ['Monday-Thursday | 7am-8pm', 'Friday | 7am-4pm', 'Saturday | Closed', 'Sunday | 2pm-8pm']
+}
 
 
 # Global variables
@@ -53,7 +68,13 @@ links = links[0].find_all('a')
 for l in links:
     dining_location_urls.append(l['href'])
 
-collect = [LOCATION.Jester_City_Limits, LOCATION.Jester_City_Market, LOCATION.Jest_A_Pizza, LOCATION.J2, LOCATION.J2_FAST, LOCATION.Kinsolving, LOCATION.Kins_Market, LOCATION.Cypress_Bend_Cafe, LOCATION.Georges_Cafe]
+print('Thanks for using UT Find Dining, the #1 tool to help you find that fine dining at UT Austin. This script was written by Nathan Chin for his EE119 Final Project')
+for j,k in LOCATION_HOURS.items():
+    print(j, 'hours:')
+    for d in k:
+        print(d)
+
+collect = [LOCATION.Jester_City_Limits, LOCATION.Jester_City_Market, LOCATION.Jest_A_Pizza, LOCATION.J2, LOCATION.J2_FAST] #LOCATION.Kinsolving, LOCATION.Kins_Market, LOCATION.Cypress_Bend_Cafe, LOCATION.Georges_Cafe]
 for count, i in enumerate(collect):
     printName(collect[count].name, '#')
     url = dining_location_urls[i.value]
